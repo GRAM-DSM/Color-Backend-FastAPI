8225eb968a9183b32edca34a83970aee4796aa9a
junkiYoon <juneki4933@gmail.com>
2021-08-06 19:31:27 +0900

    ADD report router
    app includes report router

--- server/main.py
+++ server/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 import uvicorn
 
-from server.apps import like
+from server.apps import like, report
 
 app = FastAPI()
 
 app.include_router(like.router)
+app.include_router(report.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", host="0.0.0.0", reload=True)
