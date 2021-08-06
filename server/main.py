4a46af0abcf8d938779467ff29fde4f97d398c2a
junkiYoon <juneki4933@gmail.com>
2021-08-06 19:08:18 +0900

    ADD like router
    app includes like router

--- server/main.py
+++ server/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from server.apps import like
+
 app = FastAPI()
+
+app.include_router(like.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", host="0.0.0.0", reload=True)
