cb32c3cf0aab22737b86e307c6d86107f8851f29
junkiYoon <juneki4933@gmail.com>
2021-08-06 19:05:36 +0900

    CREATE encode_token function
    create encode_token function

---
+++ server/utils/__init__.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException, status
+
+import jwt
+
+from server.config import SECRET_KEY, ALGORITHM
+
+
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if (payload["type"] == "access"):
+            return payload["sub"]
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="access token is required")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid token")
