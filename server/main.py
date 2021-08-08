814d124cedb4698a79972c8f6798b7a853f9058c
junkiYoon <juneki4933@gmail.com>
2021-08-08 15:35:33 +0900

    ADD profile router
    app includes profile router

--- server/main.py
+++ server/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 
 import uvicorn
 
-from server.apps import like, report
+from server.apps import like, report, profile
 
 app = FastAPI()
 
 app.include_router(like.router)
 app.include_router(report.router)
+app.include_router(profile.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", host="0.0.0.0", reload=True)
