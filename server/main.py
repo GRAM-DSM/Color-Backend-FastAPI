ce0183f4c2195b77cbfad39cc52856169aee9393
junkiYoon <juneki4933@gmail.com>
2021-08-10 17:58:08 +0900

    ADD admin router
    app includes admin router

--- server/main.py
+++ server/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 import uvicorn
 
-from server.apps import like, report, profile
+from server.apps import like, report, profile, admin
 
 app = FastAPI()
 
 app.include_router(like.router)
 app.include_router(report.router)
 app.include_router(profile.router)
+app.include_router(admin.router)
 
 if __name__ == '__main__':
     uvicorn.run("server.main:app", host="0.0.0.0", reload=True)
