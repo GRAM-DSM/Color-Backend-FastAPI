7f0affad401e887b2f144c39c95db3db3434ffba
junkiYoon <juneki4933@gmail.com>
2021-08-17 19:05:33 +0900

    UPDATE middleware
    create cors middleware

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
 from server.apps import like, report, profile, admin
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(like.router)
 app.include_router(report.router)
